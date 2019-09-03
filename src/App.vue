73e750f04988eb58b7dbae2f382eabe66a1a780e
Mathias Gelhausen <gelhausen@cross-solution.de>
2019-09-03 17:39:43 +0200

    Add pagination and some css.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <YawikJobList :url="url" :page="page" :count="count" :title="title"/>
+    <YawikJobList :remote="remote" :count="count" :widget-title="widgetTitle"/>
   </div>
 </template>
 
@@ -9,7 +9,7 @@ import YawikJobList from './components/YawikJobList.vue'
 
 export default {
   name: 'app',
-  props: ['url', 'title', 'page', 'count'],
+  props: ['remote', 'widget-title', 'count'],
   components: {
     YawikJobList
   }
@@ -21,7 +21,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
--- src/components/YawikJobList.vue
+++ src/components/YawikJobList.vue
@@ -1,45 +1,81 @@
 <template>
     <div id="yawik-job-list">
-        <h3>{{ title ? title : 'YAWIK Jobs List' }}</h3>
+        <h3>{{ widgetTitle ? widgetTitle : 'YAWIK Jobs List' }}</h3>
+
+        <div v-if="!error">
+            <nav class="pagination">
+                <paginate
+                    v-model="curpage"
+                    :page-count="result.totalPages"
+                    :page-range="5"
+                    :click-handler="load">
+                </paginate>
+            </nav>
+        </div>
         <div v-if="loading">Lädt...</div>
         <div v-if="error">{{ errmsg }}</div>
         <div v-if="!loading && !error">
+
             <p>
                 Page {{ result.currentPage }} / {{ result.totalPages }} |
                 Showing {{ ((result.currentPage - 1) * result.jobsPerPage) }} - {{ ((result.currentPage - 1) * result.jobsPerPage) + result.count }}
                 of {{ result.total }}
             </p>
-            <ul v-for="job in result.jobs" :key="job.id">
-                <li><a :href="job.link">{{ job.organization }}: {{ job.title }} [ {{ job.location }} ]</a></li>
-            </ul>
+            <table class="yawik-job-list-items">
+                <tbody>
+                    <tr v-for="job in result.jobs" :key="job.id">
+                        <td>
+                            <a :href="job.link">{{ job.title }}</a>
+                        </td>
+                        <td width="20%">
+                            <img v-if="job.organizationLogo" :src="job.organizationLogo" :alt="job.organization"/>
+                            <span v-else>{{ job.organization }}</span>
+                        </td>
+                        <td width="15%" v-if="job.locations.length">
+                            <div v-for="loc in job.locations" :key="loc.city">
+                                {{ loc.city ? loc.city : loc.region }}<br>
+                            </div>
+                        </td>
+                        <td width="15%" v-else>
+                            {{ job.location }}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Paginate from 'vuejs-paginate'
 
 export default {
     name: "YawikJobList",
+    components: { Paginate },
     data: function() {
         return {
             loading: true,
             error: false,
             errmsg: '',
             jobs: [],
-            result: {},
+            result: {
+                totalPages: 0
+            },
+            curpage: 1
         }
     },
-    props: ['url', 'title', 'count', 'page'],
+    props: ['remote', 'widget-title', 'count'],
     mounted: function() {
         this.load()
     },
     methods: {
-        load: function() {
+        load: function(pageNum) {
+            this.curpage = pageNum;
             this.loading = true;
             this.error = false;
 
-            if (!this.url) {
+            if (!this.remote) {
                 this.error = true;
                 this.errmsg = 'No yawik url provided. Please use the attribute "url".';
                 this.loading = false;
@@ -48,7 +84,7 @@ export default {
             var query = {
                 json: '1',
                 count: this.count ? this.count : 10,
-                page: this.page ? this.page : 1,
+                page: this.curpage ? this.curpage : 1,
             };
             /* Found on https://stackoverflow.com/questions/1714786/query-string-encoding-of-a-javascript-object
              * Converts Object to query string
@@ -56,7 +92,7 @@ export default {
              */
             var queryStr = Object.keys(query).map(k => `${k}=${encodeURIComponent(query[k])}`).join('&');
             var that = this
-            axios.get(this.url + '?' + queryStr)
+            axios.get(this.remote + '?' + queryStr)
             .then(function(response) {
                 that.jobs = response.data.jobs
                 that.result = response.data;
@@ -68,5 +104,64 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+#yawik-job-list .yawik-job-list-items {
+    border-collapse: collapse;
+}
+#yawik-job-list .yawik-job-list-items tr {
+    border-bottom: 1px solid grey;
+}
+#yawik-job-list .yawik-job-list-items tr:last-child {
+    border-bottom: 0px solid grey;
+}
+#yawik-job-list .yawik-job-list-items td {
+
+    vertical-align: middle;
+}
+#yawik-job-list .yawik-job-list-items td a {
+    display: block;
+    padding: 1.5em;
+
+}
+#yawik-job-list .yawik-job-list-items img {
+    max-width: 120px;
+    max-height: 60px;
+}
+#yawik-job-list .pagination ul {
+    display: flex;
+    list-style:none;
+}
+#yawik-job-list .pagination ul li {
+    display: flex;
+    margin: 0;
+    padding: 0;
+}
+#yawik-job-list .pagination ul li a {
+    position: relative;
+    margin: 0px 0px 0px -1px;
+    padding: 0.3em 0.7em;
+    border: 1px solid grey;
+    cursor: pointer;
+}
+#yawik-job-list .pagination ul li.active a {
+    background-color: lightblue;
+    color: blue;
+}
+#yawik-job-list .pagination ul li a:hover {
+    background-color: lightgrey;
+}
+#yawik-job-list .pagination ul li.disabled a {
+    cursor: default;
+}
+#yawik-job-list .pagination ul li.disabled a:hover {
+    background-color: white;
+}
+#yawik-job-list .pagination ul li:first-child a {
+    border-top-left-radius: 0.25em;
+    border-bottom-left-radius: 0.25em;
+}
+#yawik-job-list .pagination ul li:last-child a {
+    border-top-right-radius: 0.25em;
+    border-bottom-right-radius: 0.25em;
+}
 </style>
